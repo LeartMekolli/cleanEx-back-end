c5107648def8fed9d4e5c990ac4d2b653880b256
EagleGazii <gazmorabdiu@hotmail.com>
2021-03-18 23:58:37 +0100

    add some data to jobs table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,36 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+
+        // create job list here
+        $jsonString = file_get_contents(base_path('app\resources\Qytetet_e_Kosoves_Database.json'));
+
+        $data = json_decode($jsonString, true);
+
+        foreach($data as $item){
+            $checkItem = \App\Models\City::where('city',$item['city'])->first();
+            
+            if($checkItem == null){
+                \App\Models\City::create(['city'=>$item['city']]);
+            }
+            
+            
+        }
+
+        $jsonJobString = file_get_contents(base_path('app\resources\jobs.json'));
+
+        $job_data = json_decode($jsonJobString, true);
+
+        foreach($job_data as $item){
+            $checkItem = \App\Models\Job::where('name',$item['name'])->first();
+            if($checkItem == null){
+                \App\Models\Job::create(['name'=>$item['name']]);
+            }
+            
+            
+        }
+
+
     }
 }
